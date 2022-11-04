56efd51259b5f238a9c476fbcfff79a5843eb380
anupama <anupamaunni612@gmail.com>
2022-11-04 14:22:37 +0530

    after creating new branch

--- reseller_app/models.py
+++ reseller_app/models.py
@@ -13,6 +13,7 @@ class Reseller(models.Model):
     s_acholdername = models.CharField(max_length=50)
     password = models.CharField(max_length=30)
     s_pic = models.ImageField(upload_to='reseller/')
+    s_status = models.IntegerField(default=1)
 
 class Product(models.Model):
     p_name = models.CharField(max_length=30)
